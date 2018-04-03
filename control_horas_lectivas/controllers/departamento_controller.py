09cf29b99dafc291bfdaa1e761587df65c6388a4
William Condori Quispe <williamccondori@outlook.com>
2018-04-03 11:06:27 -0500

    Se recuperan archivos

--- control_horas_lectivas/controllers/departamento_controller.py
+++ control_horas_lectivas/controllers/departamento_controller.py
@@ -3,7 +3,6 @@ from django.views.generic import View
 from django.shortcuts import render, redirect
 
 class DepartamentoController(View):
-
     def post(self, request, *args, **kwargs):
         id_plan_estudio = request.session.get('id_plan_estudio', False)
         if id_plan_estudio is not False:
